3ef36fe9e452fb07af881e2215b7a628e7fa540a
KWY <nivekyang12014@gmail.com>
2025-04-14 21:02:26 +0800

    feat: Create routes and pages for books

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "books"
+
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name = "index"),
+    path("new", views.new, name = "new")
 ]
--- books/views.py
+++ books/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return render(req, "books/index.html")
+
+def new(req):
+    return render(req, "books/new.html")
