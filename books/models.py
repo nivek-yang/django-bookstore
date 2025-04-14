a475e50ef69c64b18b7e5164e1c0b9c9ac883285
KWY <nivekyang12014@gmail.com>
2025-04-14 21:16:46 +0800

    feat: Build models for books and register to admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=50)
+    publication_date = models.DateField()
+    price = models.PositiveIntegerField()
+    page = models.PositiveIntegerField()
+
+
+
+
+
+
+
