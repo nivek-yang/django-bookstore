5dc787a459f9d1be59014737d03b026460844127
KWY <nivekyang12014@gmail.com>
2025-04-14 21:35:53 +0800

    feat: Show book list in book home page

--- books/views.py
+++ books/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(req):
-    return render(req, "books/index.html")
+    books = Book.objects.all()
+    return render(req, "books/index.html", {"books": books})
 
 def new(req):
     return render(req, "books/new.html")
