a1f37bd10dda8a3a39ec1067cbab6e4970b8d250
KWY <nivekyang12014@gmail.com>
2025-04-15 18:04:11 +0800

    feat: Implement book creation and detail view functionality

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,6 @@ app_name = "books"
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("new", views.new, name = "new")
+    path("/new", views.new, name = "new"),
+    path("/<int:id>", views.show, name="show")
 ]
--- books/views.py
+++ books/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 
 # Create your views here.
 def index(req):
-    books = Book.objects.all()
-    return render(req, "books/index.html", {"books": books})
+    if req.POST:
+        title = req.POST["title"]
+        publisher = req.POST["publisher"]
+        publication_date = req.POST["publication_date"]
+        price = req.POST["price"]
+        page = req.POST["page"]
+
+        book = Book.objects.create(
+            title=title,
+            publisher=publisher,
+            publication_date=publication_date,
+            price=price,
+            page=page
+        )
+
+        return redirect("books:show", book.id)
+
+    else:
+        books = Book.objects.all()
+        return render(req, "books/index.html", {"books": books})
 
 def new(req):
     return render(req, "books/new.html")
+
+def show(req, id):
+    book = get_object_or_404(Book, pk=id)
+    return render(req, "books/show.html", {"book": book})
